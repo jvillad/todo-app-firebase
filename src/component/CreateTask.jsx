3d2433af31ed289715f0c573373306a1f36e56ea
jvillad <jyvillad@gmail.com>
2023-01-03 16:34:13 +1100

    adding task to db and query db to display tasks

--- src/component/CreateTask.jsx
+++ src/component/CreateTask.jsx
@@ -1,17 +1,35 @@
-import { useState } from 'react';
-import Task from './Task';
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from 'react';
+import { addDoc, collection, onSnapshot, query } from 'firebase/firestore';
+import { db } from '../config/firebase';
+import DisplayTask from './DisplayTask';
+
 function CreateTask() {
   const [tasks, setTasks] = useState([]);
   const [userTask, setUserTask] = useState('');
+  const todosDb = collection(db, 'todos');
 
-  const addTask = () => {
-    const newTask = {
-      id: Date.now(),
-      theTask: userTask,
-      status: 'not-done',
-    };
-    setTasks([...tasks, newTask]);
+  // Read todos from firebase
+  useEffect(() => {
+    const dbQuery = query(collection(db, 'todos'));
+    const unsub = onSnapshot(dbQuery, (querySnapshot) => {
+      const todos = [];
+      querySnapshot.forEach((doc) => {
+        todos.push({ ...doc.data(), id: doc.id });
+      });
+      setTasks(todos);
+    });
+    return () => unsub();
+  }, []);
+
+  // Add new task to db
+  const addToDb = async (todo) => {
+    await addDoc(todosDb, {
+      task: userTask,
+      completed: false,
+    });
   };
+
   const handleUserTask = (ev) => {
     setUserTask(ev.target.value);
   };
@@ -19,9 +37,9 @@ function CreateTask() {
   return (
     <div className="task">
       <input type="text" onChange={handleUserTask} />
-      <button onClick={addTask}>Add</button>
+      <button onClick={addToDb}>Add</button>
       {tasks.map((task) => (
-        <Task key={task.id} newTask={task.theTask} status={task.status} />
+        <DisplayTask key={task.id} newTask={task.task} status={task.status} />
       ))}
     </div>
   );

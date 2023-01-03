fffec73fe36396d087a6b6775e446ac308836d86
jvillad <jyvillad@gmail.com>
2023-01-03 12:49:54 +1100

    set prop-types: 0

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,5 +14,5 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['react', 'prettier'],
-  rules: { 'react/react-in-jsx-scope': 'off' },
+  rules: { 'react/react-in-jsx-scope': 'off', 'react/prop-types': 0 },
 };

50cb39f4ff77bdacbb4efe00da19e91ae03bf0e0
jvillad <jyvillad@gmail.com>
2023-01-10 08:54:52 +1100

    login page

---
+++ src/component/Login.jsx
@@ -0,0 +1,31 @@
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { FcGoogle } from 'react-icons/fc';
+import { auth } from '../config/firebase';
+import { useNavigate } from 'react-router-dom';
+
+function Login() {
+  const [user, loading] = useAuthState(auth);
+  const googleProvider = new GoogleAuthProvider();
+  const navigate = useNavigate();
+  const googleLogin = async () => {
+    try {
+      // eslint-disable-next-line no-unused-vars
+      const result = await signInWithPopup(auth, googleProvider);
+
+      navigate('/todo');
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.log(error);
+    }
+  };
+  return (
+    <div className="login-container">
+      <h2>Be productive today!</h2>
+      <button onClick={googleLogin}>
+        <FcGoogle /> Sign in with Google
+      </button>
+    </div>
+  );
+}
+export default Login;

94a40b1711e318dbba7801b3b56e66d04a11669c
jvillad <jyvillad@gmail.com>
2023-01-11 07:51:26 +1100

    Linked Nav

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './component/Login';
 import PrivateRoute from './config/PrivateRoute';
 import NotFound from './component/NotFound';
+import Nav from './nav/Nav';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Nav />
         <Routes>
           <Route path="/" element={<Login />} />
           <Route element={<PrivateRoute />}>

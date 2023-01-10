7f373e100a48253acdddee1ed9bf90ea78c2cb36
jvillad <jyvillad@gmail.com>
2023-01-11 09:51:26 +1100

    re-route user to /todo when logged in

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,4 +1,5 @@
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { FcGoogle } from 'react-icons/fc';
 import { auth } from '../config/firebase';
 import { useNavigate } from 'react-router-dom';
@@ -6,17 +7,19 @@ import { useNavigate } from 'react-router-dom';
 function Login() {
   const googleProvider = new GoogleAuthProvider();
   const navigate = useNavigate();
+  const [user, loading] = useAuthState(auth);
   const googleLogin = async () => {
     try {
       // eslint-disable-next-line no-unused-vars
       const result = await signInWithPopup(auth, googleProvider);
-
       navigate('/todo');
     } catch (error) {
       // eslint-disable-next-line no-console
       console.log(error);
     }
   };
+  if (loading) return;
+  if (user) navigate('/todo');
   return (
     <div className="login-container">
       <h2>Be productive today!</h2>

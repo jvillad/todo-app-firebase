0c3b7cff216eb20e43d277e9ab257f94294b2918
jvillad <jyvillad@gmail.com>
2023-01-12 14:42:59 +1100

    remove useEffect and useState

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../config/firebase';
 export const DataContext = createContext();

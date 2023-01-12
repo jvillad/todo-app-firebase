d8f4b1c6bc7ce1d6facff8c2950b7e8c7e851cc2
jvillad <jyvillad@gmail.com>
2023-01-12 14:42:04 +1100

    link dataprovider to the app

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,26 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './component/Login';
 import PrivateRoute from './config/PrivateRoute';
 import NotFound from './component/NotFound';
+import { DataProvider } from './context/DataContext';
+
 import Nav from './nav/Nav';
 
 function App() {
   return (
-    <BrowserRouter>
+    <DataProvider>
       <div className="App">
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route element={<PrivateRoute />}>
-            <Route path="/todo" element={<CreateTask />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <BrowserRouter>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route element={<PrivateRoute />}>
+              <Route path="/todo" element={<CreateTask />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
       </div>
-    </BrowserRouter>
+    </DataProvider>
   );
 }
 

e8c26efa4e06cda0d36d9fe91231b06ba338da97
jvillad <jyvillad@gmail.com>
2023-01-12 14:41:37 +1100

    context provider for login and nav

---
+++ src/context/DataContext.jsx
@@ -0,0 +1,11 @@
+import { createContext, useState, useEffect } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../config/firebase';
+export const DataContext = createContext();
+
+// eslint-disable-next-line react/prop-types
+export function DataProvider({ children }) {
+  const [user, loading] = useAuthState(auth);
+  if (loading) return;
+  return <DataContext.Provider value={user}>{children}</DataContext.Provider>;
+}

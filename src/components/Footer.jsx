c87bea0a18048d1d953fe173d5acdd63c95de08d
jvillad <jyvillad@gmail.com>
2023-04-25 00:05:40 +1000

    added a footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,13 @@
-function Footer(tasks) {
+const Footer = () => {
   return (
-    <div className="footer-container">
-      <section className="footer">
-        <p>
-          Todo&apos;s remaing: <span>{tasks.taskRemaining.length}</span>
+    <footer className="bottom-0 left-0 w-full bg-gray-900 py-4">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <p className="text-center text-white">
+          Made using React + Vite + Tailwind and Firebase. Powered by ☕️
         </p>
-        <p>Fueled by ☕️</p>
-      </section>
-    </div>
+      </div>
+    </footer>
   );
-}
+};
 
 export default Footer;

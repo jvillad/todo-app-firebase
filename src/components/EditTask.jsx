9d82405a6398c85e2f2b1df443ef6bde97917523
jvillad <jyvillad@gmail.com>
2023-04-25 10:41:54 +1000

    added a close btn to edit task

--- src/components/EditTask.jsx
+++ src/components/EditTask.jsx
@@ -1,4 +1,4 @@
-import { AiOutlineSave } from 'react-icons/ai';
+import { AiOutlineSave, AiOutlineClose } from 'react-icons/ai';
 import { useState, useEffect } from 'react';
 import { updateToDo } from '../helper/updateToDo';
 
@@ -24,6 +24,7 @@ function EditTask(todo) {
   const handleChange = (ev) => {
     setVal(ev.target.value);
   };
+
   const updateTask = () => {
     updateToDo(todoData);
     todo.onChange(false);
@@ -32,14 +33,27 @@ function EditTask(todo) {
   return (
     <div className="edit-task">
       <input
-        className="edit-input"
+        className="p-2 edit-input"
         value={val}
         onChange={handleChange}
         ref={todo.inputRef}
       />
-      <button className="save-btn" type="submit" onClick={updateTask}>
-        {<AiOutlineSave />}
-      </button>
+      <div className="flex gap-5 pl-4">
+        <button
+          className="pt-[5px] text-[30px] border-none text-[#faa916] hover:text-orange-500"
+          type="submit"
+          onClick={updateTask}
+        >
+          {<AiOutlineSave />}
+        </button>
+        <button
+          className="pt-[5px] text-[30px] border-none text-[#eb5160] hover:text-orange-500"
+          type="submit"
+          onClick={() => todo.onChange(false)}
+        >
+          {<AiOutlineClose />}
+        </button>
+      </div>
     </div>
   );
 }
